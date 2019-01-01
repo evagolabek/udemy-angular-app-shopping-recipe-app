7d100b4d2245e007c9692ab21816ee43e33b7cd4
evagolabek <golabek.eva@gmail.com>
2019-01-01 14:55:04 +0100

    recipe-list-html-template

--- udemy-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ udemy-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'some test recipe', 'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/enchiladas-aux-legumes-1a1102aa.jpg')
+  ];
 
   constructor() { }
 
--- udemy-project/src/app/recipes/recipes.model.ts
+++ udemy-project/src/app/recipes/recipes.model.ts
@@ -1,6 +1,3 @@
-
-//
-
 export class Recipe {
   public name: string;
   public description: string;
