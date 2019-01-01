a39e3740f792295671cfb3b995dc716ca8e16b16
evagolabek <golabek.eva@gmail.com>
2019-01-01 17:15:16 +0100

    ingredient model different way to the prev

---
+++ udemy-project/src/app/shared/ingredient.model.ts
@@ -0,0 +1,15 @@
+// nice and convenient shortcut
+export class Ingredient {
+  constructor(public name: string, public amount: number) {}
+}
+
+//of the previous
+// export class Ingredient {
+//   public name: string;
+//   public amount: number;
+
+//   constructor(name: string, amount: number) {
+//     this.name =  name;
+//     this.amount = amount;
+//   }
+// }
