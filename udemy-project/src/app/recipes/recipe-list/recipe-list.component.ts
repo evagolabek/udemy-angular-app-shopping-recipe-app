75f6e6f69b9009bc0988bfd642e1256d1e102e20
evagolabek <golabek.eva@gmail.com>
2019-11-27 01:30:23 +0800

    recipes from recipeService

--- udemy-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ udemy-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipes.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,13 +9,12 @@ import { Recipe } from '../recipes.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A test recipe', 'some test recipe', 'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/enchiladas-aux-legumes-1a1102aa.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService ) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe){
---
+++ udemy-project/src/app/recipes/recipe.service.ts
@@ -0,0 +1,11 @@
+import { Recipe } from './recipes.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A test recipe', 'some test recipe', 'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/enchiladas-aux-legumes-1a1102aa.jpg')
+  ];
+
+  getRecipes(){
+    return this.recipes.slice();
+  }
+}
