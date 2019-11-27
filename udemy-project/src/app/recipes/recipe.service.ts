5cda35c8976f1a186ddc7d5db4c49b593d448bc7
evagolabek <golabek.eva@gmail.com>
2019-11-27 11:08:21 +0800

    adding ingredients to recipe list with service

--- udemy-project/src/app/recipes/recipe.service.ts
+++ udemy-project/src/app/recipes/recipe.service.ts
@@ -1,11 +1,23 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipes.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 
 export class RecipeService {
   recipeSelected = new EventEmitter
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'some test recipe', 'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/enchiladas-aux-legumes-1a1102aa.jpg')
+    new Recipe('A test recipe', 
+    'some test recipe', 
+    'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/enchiladas-aux-legumes-1a1102aa.jpg', 
+    [new Ingredient('meat', 1),
+    new Ingredient( 'garlic', 3)
+    ]),
+    new Recipe('A test recipe 2 ', 
+    'some test recipe 2', 
+    'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/enchiladas-aux-legumes-1a1102aa.jpg',
+    [new Ingredient('meat', 1),
+    new Ingredient( 'garlic', 3)
+    ])
   ];
 
   getRecipes(){
