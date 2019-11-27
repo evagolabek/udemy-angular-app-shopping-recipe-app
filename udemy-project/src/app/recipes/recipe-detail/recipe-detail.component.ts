b615ccb370dccd434ba75330b2d814c068b160fd
evagolabek <golabek.eva@gmail.com>
2019-11-27 11:44:12 +0800

    passing ingredients via services injectable and spread operator used

--- udemy-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ udemy-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipes.model';
+import { RecipeService } from '../recipe.service';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,9 +11,13 @@ import { Recipe } from '../recipes.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+  }
+
+  onAddToShoppingList(){
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
 
 }
--- udemy-project/src/app/recipes/recipe.service.ts
+++ udemy-project/src/app/recipes/recipe.service.ts
@@ -1,8 +1,9 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipes.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
-
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter
   private recipes: Recipe[] = [
@@ -20,7 +21,13 @@ export class RecipeService {
     ])
   ];
 
+  constructor(private slService: ShoppingListService){}
+
   getRecipes(){
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+    this.slService.addIngredients(ingredients);
   }
 }
