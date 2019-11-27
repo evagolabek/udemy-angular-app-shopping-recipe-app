c094d2dd7e9edf25923425f15aa5685119ed8eae
evagolabek <golabek.eva@gmail.com>
2019-11-27 16:23:27 +0800

    styling active links

--- udemy-project/src/app/recipes/recipe.service.ts
+++ udemy-project/src/app/recipes/recipe.service.ts
@@ -7,17 +7,17 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipeSelected = new EventEmitter
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 
-    'some test recipe', 
+    new Recipe('Enchiladas', 
+    'Hot enchiladas', 
     'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/enchiladas-aux-legumes-1a1102aa.jpg', 
-    [new Ingredient('meat', 1),
-    new Ingredient( 'garlic', 3)
+    [new Ingredient('wraps', 1),
+    new Ingredient( 'some meat', 3)
     ]),
-    new Recipe('A test recipe 2 ', 
-    'some test recipe 2', 
-    'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/enchiladas-aux-legumes-1a1102aa.jpg',
+    new Recipe('Schnitzel', 
+    'Super tasty schnitzel', 
+    'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
     [new Ingredient('meat', 1),
-    new Ingredient( 'garlic', 3)
+    new Ingredient( 'fries', 20)
     ])
   ];
 
