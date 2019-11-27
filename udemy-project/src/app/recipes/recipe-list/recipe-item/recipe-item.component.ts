edd99c0ef5dabd880b132681ea933a13d4276c93
evagolabek <golabek.eva@gmail.com>
2019-11-27 15:57:28 +0800

    configuring routes parameters

--- udemy-project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ udemy-project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { Recipe } from '../../recipes.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -11,13 +10,9 @@ export class RecipeItemComponent implements OnInit {
   //biniding from outside
   @Input() recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
   }
 
-  onSelected(){
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
 
 }
